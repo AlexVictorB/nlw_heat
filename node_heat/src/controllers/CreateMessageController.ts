aaa608009d1095eac0c7812ccbddf6b671bf43e6
AlexVictorB <alexvictorbarreto@hotmail.com>
2021-10-22 13:39:43 -0300

    .

--- node_heat/src/controllers/CreateMessageController.ts
+++ node_heat/src/controllers/CreateMessageController.ts
@@ -1,24 +1,17 @@
-import { request, response } from 'express';
-import { Request, Response } from 'express';
-import { CreateMessageService } from '../services/CreateMessageService';
-
-
-
+import { Request, Response } from "express";
+import { CreateMessageService } from "../services/CreateMessageService";
 
 class CreateMessageController {
-    async handle(request: Request, response: Response) {
-        const { message } = request.body;
-        
-        const { user_id } = request.body;
+  async handle(request: Request, response: Response) {
+    const { message } = request.body;
+    const { user_id } = request.body;
 
-        const service = new CreateMessageService()
+    const service = new CreateMessageService();
 
-        const result = await service.execute(message, user_id)
+    const result = await service.execute(message, user_id);
 
-
-        return response.json(result)
-
-    }
+    return response.json(result);
+  }
 }
 
-export { CreateMessageController }
+export { CreateMessageController };
